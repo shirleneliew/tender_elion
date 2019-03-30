cf8b3e7669af6fb4f90a224312e1065ab2b9c643
Alvin Ng <alvinng15@gmail.com>
2019-03-30 21:59:50 +0800

    fixed route

--- tender/userflow/views.py
+++ tender/userflow/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def onboarding(request):
-    return render(request, 'userflow/index.html')
+    return render(request, 'userflow/onboarding.html')
 
 
 
