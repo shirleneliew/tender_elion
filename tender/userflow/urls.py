ea632a7fed852ac8aee2e4b07669d789d9d2beab
deepan <blinduck@gmail.com>
2019-03-31 14:33:30 +0800

    updating views

--- tender/userflow/urls.py
+++ tender/userflow/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 app_name = 'userflow'
 urlpatterns = [
+    path('test', views.test, name='test'),
     path('onboarding', views.onboarding, name='onboarding'),
     path('experience', views.experience, name='experience'),
     path('symptom/new', views.symptom_new, name='symptom_new'),
--- tender/userflow/views.py
+++ tender/userflow/views.py
@@ -1,27 +1,55 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+import pandas as pd
+from datetime import timedelta
+
+df = pd.read_pickle('userflow/processed_symptoms.pkl')
+
+
+def get_distribution(cl, pl):
+    similar_people = df[
+        (df.cycle_length_initial >= timedelta(days=cl - 1)) &
+        (df.cycle_length_initial <= timedelta(days=cl + 1)) &
+        (df.period_length_initial >= timedelta(days=pl - 1)) &
+        (df.period_length_initial <= timedelta(days=pl + 1))
+    ]
+    return similar_people
+
+
+def test(request):
+    # return df[df.cycle]
+    ss = get_distribution(26, 4)
+    return HttpResponse(ss.shape[0])
+
 
 # Create your views here.
 def onboarding(request):
     return render(request, 'userflow/onboarding.html')
 
+
 def experience(request):
     # process the get
     print(request.GET)
+    period_length = request.GET.get('period_length')
+    cycle_length = request.GET.get('cycle_length')
 
     mock = {
-      'x_other_women': 1234,
-      'total_women': 5000,
-      1: {'pain': 3, 'soreness': 6},
-      10: {'soreness': 8}
-    } 
+        'x_other_women': 1234,
+        'total_women': 6729,
+        1: {'pain': 3, 'soreness': 6},
+        10: {'soreness': 8}
+    }
     data = mock
     return render(request, 'userflow/experience.html', context=data)
+
 
 def symptom_new(request):
     return render(request, 'userflow/symptom/new.html')
 
+
 def symptom_result(request):
     return render(request, 'userflow/symptom/result.html')
+
 
 def report(request):
     return render(request, 'userflow/report.html')
